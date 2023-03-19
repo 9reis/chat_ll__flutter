785f9ad22abf309baa28bcb713e4cc564845b99a
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-19 15:35:04 -0300

    Logia Auth #3 - Att estaticos

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -4,8 +4,16 @@ import 'package:chat_ll__flutter/core/models/chat_user.dart';
 import 'package:chat_ll__flutter/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  // Independente da instancia de AuthMockService
+  // Vai receber os mesmos users
+  static Map<String, ChatUser> _users = {};
+
+  //Static pois ñ faz sentido ter multiplos usuários
+  // dentro da mesma aplicação
+  static ChatUser? _currentUser;
+
   ChatUser? get currentUser {
-    return null;
+    return _currentUser;
   }
 
   Stream<ChatUser?> get userChanges {}
