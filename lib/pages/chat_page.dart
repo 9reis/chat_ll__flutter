fa4ada87754991e451f312df3aa484824eee0f8f
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-19 23:30:14 -0300

    Chate page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_ll__flutter/components/messages.dart';
+import 'package:chat_ll__flutter/components/new_message.dart';
 import 'package:chat_ll__flutter/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,50 @@ class ChatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Chat Page'),
-            TextButton(
-              child: Text('Logout'),
-              onPressed: () {
+      appBar: AppBar(
+        elevation: 20,
+        title: Text('9reis chat'),
+        centerTitle: true,
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('Sair')
+                    ],
+                  ),
+                ),
+              )
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
                 AuthService().logout();
-              },
-            )
+              }
+            },
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
           ],
         ),
       ),
