9d95f6b0aa99b6c6f3bbdbb956cd88a126bc5a7d
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-21 16:31:55 -0300

    Icone Notification - Navegando para pag de notificação

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat_ll__flutter/components/messages.dart';
 import 'package:chat_ll__flutter/components/new_message.dart';
 import 'package:chat_ll__flutter/core/services/auth/auth_service.dart';
+import 'package:chat_ll__flutter/core/services/notification/chat_notification_service.dart';
+import 'package:chat_ll__flutter/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatePage extends StatelessWidget {
   const ChatePage({super.key});
@@ -14,36 +17,71 @@ class ChatePage extends StatelessWidget {
         title: Text('9reis chat'),
         centerTitle: true,
         actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+                items: [
+                  DropdownMenuItem(
+                    value: 'logout',
+                    child: Container(
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.exit_to_app,
+                            color: Colors.black87,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Sair')
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+                onChanged: (value) {
+                  if (value == 'logout') {
+                    AuthService().logout();
+                  }
+                },
+              ),
             ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Container(
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.black87,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('Sair')
-                    ],
+          ),
+          Stack(children: [
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) {
+                      return NotificationPage();
+                    },
+                  ),
+                );
+              },
+            ),
+            Positioned(
+              top: 5, //12
+              right: 5,
+              child: CircleAvatar(
+                maxRadius: 9,
+                backgroundColor: Colors.red,
+                child: Text(
+                  '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              )
-            ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().logout();
-              }
-            },
-          )
+              ),
+            )
+          ]),
         ],
       ),
       body: SafeArea(
---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Notificações'),
+      ),
+      body: Center(
+        child: Text('Notificacoes'),
+      ),
+    );
+  }
+}
